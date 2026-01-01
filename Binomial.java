caa7de2f75a69c0cda63ebd0c72884bdb8932d01
Omer Hoz <omer.hoz@post.runi.ac.il>
2026-01-01 16:59:03 +0200

    finalChanges2

--- Binomial.java
+++ Binomial.java
@@ -23,7 +23,7 @@ public class Binomial {
 	
 	// Computes the Binomial function, efficiently
 	public static int binomial(int n, int k) {
-		int[][] memo = new int[n + 1][k + 1];
+		long[][] memo = new long[n + 1][k + 1];
 
     	for (int i = 0; i <= n; i++) {
         	for (int j = 0; j <= k; j++) {
@@ -34,7 +34,7 @@ public class Binomial {
     return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long[][] memo) {
 		if (k > n) {
         	return 0;
     }
